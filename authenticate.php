d1ecc214d95637448ed5c5cc8d01b76a12dd793e
Jonathan Nicholls <jonathanrnicholls@gmail.com>
2020-11-04 14:38:04 +0800

    Added basic layout of website
    This is the basic layout of the website I'll be using. I got it from the CodeShack guide on login systems: https://codeshack.io/secure-login-system-php-mysql/#creatingthedatabaseandsettinguptables

---
+++ authenticate.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+// Define our variables, which are all the parameters needed to acsess the database
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = ''; // Put whatever the database password is between the apostrephes.
+$DATABASE_NAME = 'compsci_ia';
+// Connect to the database
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if (mysqli_connect_errno()) {
+  // If there is an error with the connection, stop the script and display the error
+  exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+
+// Check to see if the data from the login form was submitted, isset() will check if the data actually exists
+if (!isset($_POST['username'], $_POST['password'])) {
+  // If this runs, it means that there wasn't available data in the username and password fields
+  exit('Please fill both the username and password fields!');
+}
+
+// Prepare our SQL. This prevents an SQL Injection.
+if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
+  // Bind parameters (s = string, i = int, etc)
+  $stmt->bind_param('s', $_POST['username']); // Takes whatever was in the 'username' field and assigns it to the parameter s
+  $stmt->execute(); // Executes the SQL statement held in $stmt, so we can check to see if the account exists in the database
+  $stmt->store_result();
+  // And now the fun begins
+  if($stmt->num_rows > 0) { // Checks if the query returns any results. Essentially, it checks to see if the username exists
+    $stmt->bind_result($id, $password);
+    $stmt->fetch();
+    // Account exists, now we verify the password.
+    // Note: remember to use password_hash in registration file to store hashed passwrods
+    if (password_verify($_POST["password"], $password)) {
+      // Password verifeid.
+      // Create sesssions so we know the user is logged in, they basicallya ct like cookies
+      session_regenerate_id();
+      $_SESSION['loggedin'] = TRUE;
+      $_SESSION['name'] = $_POST['username'];
+      $_SESSION['id'] = $id;
+      header('Location: home.php');
+    } else {
+      // Incorrect password
+      echo "Incorrect username and/or password.";
+    }
+  } else {
+    // Incorrect username
+    echo "Incorrect username and/or password.";
+  }
+  $stmt->close();
+}
+?>
---
+++ profile.php
@@ -0,0 +1,65 @@
+<?php
+session_start();
+// If the user isn't logged in, redirect them to the login page:
+if (!isset($_SESSION['loggedin'])) {
+  header('Location: index.html');
+  exit;
+}
+
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = ''; // Put whatever the database password is between the apostrephes.
+$DATABASE_NAME = 'compsci_ia';
+
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if (mysqli_connect_errno()) {
+  exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+// Since neither the email nor password was stored in the sessions, we have to get it from the database
+$stmt = $con->prepare('SELECT password, email FROM accounts WHERE id = ?');
+// We can use the account ID that we have stored in the session to get the account info
+$stmt->bind_param('i', $_SESSION['id']);
+$stmt->execute();
+$stmt->bind_result($password, $email);
+$stmt->fetch();
+$stmt->close();
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Profile</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+  </head>
+  <body class="loggedin">
+    <nav class="navtop">
+      <div>
+        <h1>Garden Logger</h1>
+        <a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
+        <a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+      </div>
+    </nav>
+    <div class="content">
+      <h2>Profile</h2>
+      <div>
+        <p>Your account details are below:</p>
+        <table>
+          <tr>
+            <td>Username:</td>
+            <td><?=$_SESSION['name']?></td>
+          </tr>
+          <tr>
+            <td>Password:</td>
+            <td><?=$password?></td>
+          </tr>
+          <tr>
+            <td>Email:</td>
+            <td><?=$email?></td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </bodY>
+</html>
