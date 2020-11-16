9c871023e116642d509581d2300f86b41881daf6
JohnyNich <jonathanrnicholls@gmail.com>
2020-11-16 08:55:50 +0800

    Added weather widget. Weather widget courtesy of weatherwidget.io.

--- home.php
+++ home.php
@@ -30,6 +30,11 @@ if (!isset($_SESSION['loggedin'])) {
     </nav>
     <div class="content">
       <h2>Home Page</h2>
+      <!-- Weather widget -->
+      <a class="weatherwidget-io" href="https://forecast7.com/en/22d28114d16/hong-kong-island/" data-label_1="HONG KONG" data-label_2="WEATHER" data-theme="original" >HONG KONG WEATHER</a>
+      <script>
+        !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+      </script>
       <p>Welcome back, <?=$_SESSION['name']?>!</p>
     </div>
   </body>
